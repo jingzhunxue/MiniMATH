5bafe485dd13d85aaac0d119df7f42901e05735e
Happen\windows-work <happenmass@gmail.com>
2024-04-01 17:05:13 +0800

    add eval code

---
+++ MATH23K/Math23K_eval.py
@@ -0,0 +1,145 @@
+import sys
+from pathlib import Path
+
+# 添加 frontend 目录的父目录到 sys.path
+project_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_dir))
+
+import os
+import torch
+from tokenizer.qwen72B.tokenization_qwen import QWenTokenizer
+
+from model.modelv0 import ModelArgs, Transformer
+
+import json
+import random
+from tqdm import tqdm
+
+if __name__ == "__main__":
+
+    print("当前工作目录:", os.getcwd())
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cpu")
+    
+    print(f"Using device: {device}")
+
+    tokenizer = QWenTokenizer.from_pretrained("tokenizer/qwen72B", local_files_only=True)
+
+    print(f"Vocab size: {tokenizer.vocab_size}")
+
+
+
+    model_args = dict(
+        dim=0,
+        n_layers=0,
+        n_heads=0,
+        n_kv_heads=0,
+        vocab_size=0,
+        multiple_of=0,
+        max_seq_len=0,
+        dropout=0,
+    )
+
+    out_dir = "out"
+    ckpt = "" ## your ckpt name here
+    print(f"inference from {out_dir}")
+
+    # resume training from a checkpoint.
+
+    ckpt_path = os.path.join(out_dir, ckpt+".pt")
+    checkpoint = torch.load(ckpt_path, map_location="cpu")
+    checkpoint_model_args = checkpoint["model_args"]
+
+    for k in ["dim", "n_layers", "n_heads", "n_kv_heads", "vocab_size", "multiple_of", "max_seq_len"]:
+        model_args[k] = checkpoint_model_args[k]
+        print(f"model_args[{k}] = {model_args[k]}")
+    
+    # create the model
+    gptconf = ModelArgs(**model_args)
+    model = Transformer(gptconf)
+
+    state_dict = checkpoint["model"]
+
+    unwanted_prefix = "module._orig_mod."
+    for k, v in list(state_dict.items()):
+        if k.startswith(unwanted_prefix):
+            state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
+
+    model.load_state_dict(state_dict)
+    
+    iter_num = checkpoint["iter_num"]
+    best_val_loss = checkpoint["best_val_loss"]
+
+    model = model.to(device)
+    model = model.half()
+    model.eval()
+
+    train_json = {}
+    
+    import re
+
+    pattern = re.compile(r"#### (\d+)")
+
+
+    #获取随机评估数据 math 23k
+    with open('MATH23K/math23k_test.json', 'r', encoding='utf-8') as f:
+        datas = json.load(f)
+        for data in datas:
+            train_json[data["original_text"]] = data["ans"]
+    
+    problem_nums = 1000
+    bar = tqdm(total=problem_nums)
+    #打乱df顺序
+    train_json_list = list(train_json.items())
+    sampled_train_json_list = random.sample(train_json_list, problem_nums)
+    df_topn = dict(sampled_train_json_list)
+
+    total_score = 0
+    total_correct = 0
+    total_count = 0
+    top_k = 1
+    top_p = 1.0
+    with open("MATH23K/result.json", "w", encoding="UTF-8") as f:
+        for q, a in df_topn.items():
+            messages = [
+                {
+                    "role": "user",
+                    "content": q
+                }
+            ]
+            output_text = model.generate_messages(tokenizer, messages, max_new_tokens=512, temperature=0.01, top_p=top_p, device=device)
+
+            matches = re.findall(r'-?\d+(?:\.\d+)?', output_text)
+            
+            if len(matches) > 0:
+                ans = matches[-1]
+                if ans == a:
+                    result = {
+                        "result": "正确",
+                        "score": "100"
+                    }
+                else:
+                    result = {
+                        "result": "错误",
+                        "score": "0"
+                    }
+            total_count += 1
+            result["question"] = q
+            result["answer"] = output_text
+            result["traing_answer"] = a
+            json.dump(result, f, indent=4, ensure_ascii=False)
+            f.write("\n")
+            
+            total_score += int(result["score"])
+            
+            if result["result"] == "正确":
+                total_correct += 1
+            
+            f.flush()
+            
+            bar.update(1)
+            bar.set_postfix_str(f"total score: {total_score}, evarage score: {total_score/total_count}, correct rate: {total_correct/total_count}")
+
+        f.write(f"total score: {total_score}, evarage score: {total_score/total_count}, correct rate: {total_correct/total_count}")
+        print(f"total score: {total_score}, evarage score: {total_score/total_count}")
---
+++ MMLU/MMLU_eval.py
@@ -0,0 +1,116 @@
+import sys
+from pathlib import Path
+import pandas as pd
+
+# 添加 frontend 目录的父目录到 sys.path
+project_dir = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_dir))
+
+import os
+import torch
+from tokenizer.qwen72B.tokenization_qwen import QWenTokenizer
+
+from model.modelv0 import ModelArgs, Transformer
+
+import json
+
+import re
+
+from tqdm import tqdm
+
+def get_csv_files(dir):
+    return [f for f in os.listdir(dir) if f.endswith('.csv')]
+
+print("当前工作目录:", os.getcwd())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(f"Using device: {device}")
+
+tokenizer = QWenTokenizer.from_pretrained("tokenizer/qwen72B", local_files_only=True)
+
+print(f"Vocab size: {tokenizer.vocab_size}")
+
+model_args = dict(
+    dim=0,
+    n_layers=0,
+    n_heads=0,
+    n_kv_heads=0,
+    vocab_size=0,
+    multiple_of=0,
+    max_seq_len=0,
+    dropout=0,
+)
+
+out_dir = "out"
+ckpt = "" ## your ckpt name here
+print(f"inference from {out_dir}")
+
+
+ckpt_path = os.path.join(out_dir, ckpt+".pt")
+checkpoint = torch.load(ckpt_path, map_location="cpu")
+checkpoint_model_args = checkpoint["model_args"]
+
+
+for k in ["dim", "n_layers", "n_heads", "n_kv_heads", "vocab_size", "multiple_of", "max_seq_len"]:
+    model_args[k] = checkpoint_model_args[k]
+    print(f"model_args[{k}] = {model_args[k]}")
+
+# create the model
+gptconf = ModelArgs(**model_args)
+model = Transformer(gptconf)
+state_dict = checkpoint["model"]
+
+unwanted_prefix = "module._orig_mod."
+for k, v in list(state_dict.items()):
+    if k.startswith(unwanted_prefix):
+        state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
+model.load_state_dict(state_dict)
+
+
+model = model.half()
+model.eval()
+model = model.to(device)
+
+
+csv_files = get_csv_files('MMLU/data/val')
+
+top_p = 1.0
+top_k = 1
+
+total_count = 0
+correct_count = 0
+
+bar = tqdm()
+
+with open('MMLU/result.json', 'w', encoding="UTF-8") as f:
+    for csv_file in csv_files:
+            df = pd.read_csv(f'MMLU/data/val/{csv_file}', header=None)
+            for index, row in df.iterrows():
+                p, chioce_a, chioce_b, chioce_c, chioce_d, answer = row
+                problem = "Answer the single chioce question：\n response like<The answer is A at last>\n" + p + "\n" + "A. " + chioce_a + "\n" + "B. " + chioce_b + "\n" + "C. " + chioce_c + "\n" + "D. " + chioce_d
+                answer = answer
+                
+                messages=[
+                    {
+                        "role": "user",
+                        "content": problem
+                    }
+                ]
+
+                output_text = model.generate_messages(tokenizer, messages, max_new_tokens=512, temperature=0.01, top_p=top_p, device=device)
+                matches = re.findall(r"[A-D]", output_text)
+                if len(matches) == 0:
+                    print(f"Error: {output_text}")
+                    continue
+                total_count += 1
+                if matches[-1] == answer:
+                    correct_count += 1
+                    f.write(json.dumps({"结果":"正确", "question": problem, "answer": output_text, "standard":answer}, ensure_ascii=False) + "\n")
+                else:
+                    f.write(json.dumps({"结果":"错误", "question": problem, "answer": output_text, "standard":answer}, ensure_ascii=False) + "\n")
+                bar.update(1)
+                bar.set_postfix_str(f"Accuracy: {correct_count / total_count:.4f}")
+
+
+                f.flush()
